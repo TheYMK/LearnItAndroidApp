f1a6aaf1a6a6effb8be414188aad76423e6a3fbc
Kaym <yimkaboss@gmail.com>
2019-07-21 21:40:41 +0800

    Implemented bottom navigation view in HSK part with fragments

--- app/src/main/java/com/kaymkassai/learnit/HskFragment.java
+++ app/src/main/java/com/kaymkassai/learnit/HskFragment.java
@@ -16,40 +16,44 @@ public class HskFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-//
-//        BottomNavigationView bottomNavigationView = getView().findViewById(R.id.bottom_navigation);
-//        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
-//
-//        //I added this if statement to keep the selected fragment when rotating the device
-//        if (savedInstanceState == null) {
-//            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-//                    new HskFragment()).commit();
-//        }
 
+        final View v = inflater.inflate(R.layout.fragment_hsk, container, false);
 
-        return inflater.inflate(R.layout.fragment_hsk, container, false);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) v.findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getChildFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new WordListFragment()).commit();
+        }
+
+        return v;
     }
 
 
-//    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-//            new BottomNavigationView.OnNavigationItemSelectedListener() {
-//                @Override
-//                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                    Fragment selectedFragment = null;
-//
-//                    switch (item.getItemId()) {
-//                        case R.id.nav_hsklist:
-//                            selectedFragment = new WordListFragment();
-//                            break;
-//                        case R.id.nav_favorites:
-//                            selectedFragment = new FavoriteFragment();
-//                            break;
-//                        case R.id.nav_search:
-//                            selectedFragment = new SearchFragment();
-//                            break;
-//                    }
-//                    return true;
-//                }
-//            };
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_hsklist:
+                            selectedFragment = new WordListFragment();
+                            break;
+                        case R.id.nav_favorites:
+                            selectedFragment = new FavoriteFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                    }
+
+                    getChildFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
 
 }
